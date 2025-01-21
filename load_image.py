5a1c39710693b37788a84974b826186adca278c0
Vladislav <vilenskijvladisla@gmail.com>
2025-01-21 23:38:22 +0300

    реализация выхода из игры путем нажатия esc а далее кнопки

--- load_image.py
+++ load_image.py
@@ -1,6 +1,7 @@
 import pygame
 import os
 import sys
+
 def load_image(name, colorkey=None):
     fullname = os.path.join('images', name)
     # если файл не существует, то выходим
@@ -52,98 +53,90 @@ class Board:
             return None
         return x_cell, y_cell
 
-    def get_click(self, mouse_pos, screen):
+    def get_click(self, mouse_pos):
         cell = self.get_cell(mouse_pos)
         if cell:
-            return self.on_click(cell, screen)
+            self.on_click(cell)
 
-    def on_click(self, cell, screen):
+    def on_click(self, cell):
+        print(cell)
+
+
+class Field(Board):
+    # создание поля
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.board = [[0] * self.width for _ in range(self.height)]
+        # значения по умолчанию
+        self.left = 10
+        self.top = 10
+        self.cell_size_x = 30
+        self.cell_size_y = 30
+
+
+    def get_click(self, mouse_pos, screen, list, props):
+        cell = self.get_cell(mouse_pos)
+        if cell:
+            return self.on_click(cell, screen, list, props)
+
+    def on_click(self, cell, screen, list, props):
         #print(cell)
         #print(bool(self.board[cell[1]][cell[0]]))
         if not bool(self.board[cell[1]][cell[0]]):
             self.board[cell[1]][cell[0]] = 1
             rect = (cell[0] * self.cell_size_x + self.left, cell[1] * self.cell_size_y + self.top, self.cell_size_x, self.cell_size_y)
             rect = pygame.Rect(rect)
-            return Plant(screen, name='carrot', rect=rect, image=load_image('carrot_1.png'), time=10, first_image=load_image('carrot_2.png'))
+            return (Plant(screen, props[0], rect=rect, image=load_image(props[1]), first_image=load_image(props[2]),
+                          time=props[3], id=len(list) - 1),
+                    cell)
+        elif list[cell].set_time():
+            self.board[cell[1]][cell[0]] = 0
+            del list[cell]
 
 
 class Plant:
-    def __init__(self, screen, name, rect, time, image=None, count=0, first_image=None):
+    def __init__(self, screen, name, rect, time, id, image=None, count=0, first_image=None, FPS=60):
         self.screen = screen
         self.name = name
         self.rect = rect
-        self. image = image
+        self.second_image = image
         self.count = count
         self.time = time
         self.first_image = first_image
+        self.image = first_image
+        self.isGrow = True
+        self.FPS = FPS
+        self.id = id
 
-class Inventory:
-    '''ToDO: сделать запросы для БД вместо txt, нормально обработать вывод инвентаря на компьютер, вывод количества-картинка предметов, показ самого персонажа в инвентаре, показ репутации персонажа в инвентаре, показ инструментов и их урвоень в инвентаре и другие данные '''
-    def __init__(self):
-        self.all_sprites = pygame.sprite.Group()
-        self.inventory = pygame.sprite.Sprite()
-        self.inventory.image = load_image('inventory.png')
-        self.all_sprites.add(self.inventory)
-        infoObject = pygame.display.Info()
-        dis_x = infoObject.current_w
-        dis_y = infoObject.current_h
-        screen = pygame.display.set_mode((dis_x, dis_y))
-        self.all_sprites.draw(screen)
-        self.inventory.rect = self.inventory.image.get_rect()
-        self.inventory.rect.x = 100
-        self.inventory.rect.y = 150
-        with open('inventory_.txt', 'r', encoding='utf-8') as f_in:
-            self.inv = f_in.readlines()
-        self.inv = str(self.inv[0])
-        self.inv = self.inv.split(';')
-        self.monets = self.inv[0].split('=')[1]
-        self.carrots = self.inv[1].split('=')[1]
-        self.wheat = self.inv[2].split('=')[1]
-        self.bread = self.inv[3].split('=')[1]
-        self.rep = self.inv[4].split('=')[1]
-        self.watermelon = self.inv[5].split('=')[1]
-        self.potato = self.inv[6].split('=')[1]
-        self.dragon_fruit = self.inv[7].split('=')[1]
-        self.monets1 = load_image('m0net.png')
-        self.all_sprites.add(self.monets1)
-        self.monets1.rect = self.monets1.image.get_rect()
-        self.monets1.rect.x = 100
-        self.monets1.rect.y = 150
-        self.carrots1 = load_image('carrots.png')
-        self.all_sprites.add(self.carrots1)
-        self.carrots1.rect = self.carrots1.image.get_rect()
-        self.carrots1.rect.x = 100
-        self.carrots1.rect.y = 150
-        self.wheat1 = load_image('wheat.png')
-        self.all_sprites.add(self.wheat1)
-        self.wheat1.rect = self.wheat1.image.get_rect()
-        self.wheat1.rect.x = 100
-        self.wheat1.rect.y = 150
-        self.bread1 = load_image('bread.png')
-        self.all_sprites.add(self.bread1)
-        self.bread1.rect = self.bread1.image.get_rect()
-        self.bread1.rect.x = 100
-        self.bread1.rect.y = 150
-        self.rep1 = load_image('rep.png')
-        self.all_sprites.add(self.rep1)
-        self.rep1.rect = self.rep1.image.get_rect()
-        self.rep1.rect.x = 100
-        self.rep1.rect.y = 150
-        self.watermelon1 = load_image('watermelon.png')
-        self.all_sprites.add(self.watermelon1)
-        self.watermelon1.rect = self.watermelon1.image.get_rect()
-        self.watermelon1.rect.x = 100
-        self.watermelon1.rect.y = 150
-        self.potato1 = load_image('potato.png')
-        self.all_sprites.add(self.potato1)
-        self.potato1.rect = self.potato1.image.get_rect()
-        self.potato1.rect.x = 100
-        self.potato1.rect.y = 150
-        self.dragon_fruit1 = load_image('dragon_fruit.png')
-        self.dragon_fruit1.rect = self.dragon_fruit1.image.get_rect()
-        self.dragon_fruit1.rect.x = 100
-        self.dragon_fruit1.rect.y = 150
-        self.all_sprites.add(self.dragon_fruit1)
-        self.dragon_fruit1.draw(screen)
+    def set_time(self):
+        if self.count // self.FPS >= self.time:
+            self.image = self.second_image
+            self.isGrow = False
+            return True
+        return False
 
+class Inventory(Board):
+    def __init__(self, width, height, plants):
+        super().__init__(width, height)
+        self.plants = plants
 
+    def on_click(self, cell):
+        try:
+            return self.plants[cell[0] + cell[1] * self.width]
+        except IndexError:
+            pass
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        if cell:
+            return self.on_click(cell)
+
+class Settings(Board):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        print(cell)
+        if cell:
+            return cell
--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import pygame
-from load_image import load_image, Board, Inventory
+from load_image import load_image, Field, Board, Inventory, Settings
 
 pygame.init()
 
 def main():
+    wheat = ['wheat', 'wheap/wheat_2.png', 'wheap/wheat_1.png', 3, 'wheap/wheap_grains.png']
+    carrot = ['carrot', 'carrot/carrot_1.png', 'carrot/carrot_2.png', 5, 'carrot/carrot_sprout.png']
+    corn = ['corn', 'corn/corn2.png', 'corn/corn1.png', 5, 'corn/corn.png']
+    pumpkin = ['pumpkin', 'pumpkin/pumpkin2.png', 'pumpkin/pumpkin1.png', 5, 'pumpkin/pumpkin.png']
+    sunflower = ['sunflower', 'sunflower/sunflower2.png', 'sunflower/sunflower1.png', 5, 'sunflower/sunflower.png']
+    tomato = ['tomato', 'tomato/tomato2.png', 'tomato/tomato1.png', 5, 'tomato/tomato.png']
+    cucumber = ['cucumber', 'cucumber/cucumber2.png', 'cucumber/cucumber1.png', 5, 'cucumber/cucumber.png']
+
+    plants_for_inventory = [wheat.copy(), carrot.copy(), corn.copy(), pumpkin.copy(), sunflower.copy(), tomato.copy(),
+                            cucumber.copy()]
+
+    plants_images = ['wheap/wheap_grains.png', 'carrot/carrot_sprout.png', 'corn/corn.png', 'pumpkin/pumpkin.png',
+                     'sunflower/sunflower.png', 'tomato/tomato.png', 'cucumber/cucumber.png']
+
+    main_plant = wheat.copy()
+
     clock = pygame.time.Clock()
     run = True
     FPS = 60
-    mark_1 = 0
+
 
     infoObject = pygame.display.Info()
     dis_x = infoObject.current_w
@@ -20,80 +36,118 @@ def main():
 
     player_x = dis_x // 2 - 50
     player_y = dis_y // 2 - 50
-    player_speed = 4
+    player_speed = 20
 
     fon = load_image('fon.png')
     player = load_image('player.png')
 
     font = pygame.font.SysFont('Arial', 20)
 
-    field = Board(10, 8)
+    field = Field(10, 8)
     field.set_view(60, 50, 65, 67)
-    plants = []
+    plants = {}
     count = 0
+    inventory = Inventory(5, 3, plants_for_inventory)
+    inventory.set_view(275, 100, 270, 300)
+    settings = Settings(1, 1)
+    settings.set_view(350, 50, 800, 403)
+    gameplay = True
+    is_inventory = False
+    is_settings = False
+
 
     while run:
         count += 1
-        player_speed = 4
+        player_speed = 20
 
         dis.fill('black')
         dis.blit(fon, (fon_x, fon_y))
 
         key = pygame.key.get_pressed()
 
-        if key[pygame.K_LSHIFT]:
+        if key[pygame.K_LSHIFT] and gameplay:
             player_speed = 7
-        if key[pygame.K_w]:
+        if key[pygame.K_w] and gameplay:
             if dis_y // 2 - 100 > player_y and fon_y <= 0:
                 fon_y += player_speed
             elif player_y > 0:
                 player_y -= player_speed
-        if key[pygame.K_s]:
+        if key[pygame.K_s] and gameplay:
             if dis_y // 2 + 100 < player_y and fon_y >= -(2160 - dis_y):
                 fon_y -= player_speed
             elif player_y < 2060 + fon_y:
                 player_y += player_speed
-        if key[pygame.K_d]:
+        if key[pygame.K_d] and gameplay:
             if dis_x // 2 + 100 < player_x and fon_x >= -(3840 - dis_x):
                 fon_x -= player_speed
             elif player_x < 3780 + fon_x:
                 player_x += player_speed
-        if key[pygame.K_a]:
+        if key[pygame.K_a] and gameplay:
             if dis_x // 2 - 100 > player_x and fon_x <= 0:
                 fon_x += player_speed
             elif player_x > 0:
                 player_x -= player_speed
 
-        if key[pygame.K_e]:
-            if mark_1 == 0:
-                Inventory()
-                mark_1 = 1
-            elif mark_1 == 1:
-                Inventory()
-                mark_1 = 1
-
         for plant in plants:
+            plant = plants[plant]
             if plant:
                 if plant.time - plant.count // FPS > 0:
-                    plant.count += 1
-                    dis.blit(font.render(f'{plant.time - plant.count // FPS}', False, 'white'), (plant.rect[0] + 30, plant.rect[1] + 37))
-                    dis.blit(plant.first_image, plant.rect)
-                else:
-                    dis.blit(font.render('', False, 'green'), (plant.rect[0] + 30, plant.rect[1] + 37))
-                    dis.blit(plant.image, plant.rect)
+                    dis.blit(font.render(f'{plant.time - plant.count // FPS}', False, 'white'), (plant.rect[0] + 30 + fon_x, plant.rect[1] + 37 + fon_y))
+                dis.blit(plant.image, (plant.rect[0] + fon_x, plant.rect[1] + fon_y))
+                plant.set_time()
+                plant.count += 1
 
-        if pygame.mouse.get_pressed()[0]:
-            plants.append(field.get_click(pygame.mouse.get_pos(), dis))
+        if pygame.mouse.get_pressed()[0] and gameplay and main_plant:
+            print(main_plant)
+            pl = field.get_click(pygame.mouse.get_pos(), dis, plants, main_plant)
+            if pl and pl[0] and pl[1]:
+                plants[pl[1]] = pl[0]
 
         dis.blit(player, (player_x, player_y))
+        dis.blit(load_image(main_plant[4]), (dis_x - 270, -50))
 
-        # field.render(dis)
+        if is_inventory:
+            dis.blit(load_image('inventory.png'), (0, 0))
+            inventory.render(dis)
+            for x in range(inventory.width):
+                for y in range(inventory.height):
+                    try:
+                        dis.blit(load_image(plants_images[x + y * inventory.width]) ,(x * inventory.cell_size_x + inventory.left, y * inventory.cell_size_y + inventory.top))
+                    except IndexError:
+                        pass
+        if is_settings:
+            dis.blit(load_image('settings.png'), (250, 50))
+            dis.blit(load_image('exit.png'), (350, 50))
+
+        if pygame.mouse.get_pressed()[0] and is_inventory:
+            if inventory.get_click(pygame.mouse.get_pos()):
+                main_plant = inventory.get_click(pygame.mouse.get_pos())
+
+        if pygame.mouse.get_pressed()[0] and is_settings:
+            if settings.get_click(pygame.mouse.get_pos()):
+                run = False
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                plants.append(field.get_click(pygame.mouse.get_pos(), dis))
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE and is_settings:
+                is_settings = False
+                gameplay = True
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE and not is_settings:
+                is_settings = True
+                gameplay = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and gameplay and main_plant:
+                pl = field.get_click(pygame.mouse.get_pos(), dis, plants, main_plant)
+                if pl and pl[0] and pl[1]:
+                    plants[pl[1]] = pl[0]
+
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_e and is_inventory:
+                gameplay = True
+                is_inventory = False
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_e and not is_inventory:
+                gameplay = False
+                is_inventory = True
 
         pygame.display.flip()
         clock.tick(FPS)
